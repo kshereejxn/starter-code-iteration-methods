7cb0c61ca4b99ce8699786e5490d53475bc1ad50
Trinie J <jxsn00ks@gmail.com>
2020-06-30 23:16:24 -0400

    2nd commit

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ console.log(users)
 
 // Append the katas to this element:
 const main = document.querySelector('main')
+
+
+let isActive = users
+.filter (users => users.isActive === true)
+.map(users=> users.name)
+let filterh1 = document.createElement ('h1')
+filterh1.append('Kata1')
+main.append(filterh1)
+main.append(JSON.stringify(isActive))
+
+let email = users.map (users => users.email )
+let mapH1 = document.createElement ('h1')
+mapH1.append('Kata2')
+main.append(mapH1)
+main.append (JSON.stringify(email))
+
+let company = users.some (users => users.company)
+let someH1 = document.createElement ('h1')
+someH1.append('Kata3')
+main.append(someH1)
+main.append(JSON.stringify(someH1))
